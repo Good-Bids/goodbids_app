99821654fea967b7928e54d1a4eaaf4d7188eda1
nameindustry <nameindustry@gmail.com>
2023-04-06 12:55:53 -0400

    feat - adding in tmp view bid button

--- src/features/charities/CharityDetailPage.tsx
+++ src/features/charities/CharityDetailPage.tsx
@@ -1,6 +1,10 @@
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useCharityQuery } from "../../hooks/useCharity"
+import { useAuctionsQuery } from "~/hooks/useAuction";
+import { I_AuctionRowModel, I_AuctionRowCollection } from "~/utils/types/auctions";
 import { I_CharityModel } from "~/utils/types/charities";
+import Link from "next/link";
 
 /**
  * QueryLoadingDisplay
@@ -23,16 +27,42 @@ const QueryErrorDisplay = () => {
 }
 
 /**
- * AuctionsListView
- * Will update this method after the auction hooks file gets commited
+ * AuctionListRowView
+ * Component container for the AuctionModel display
+ * separated for rendering/windowing and isolated updates in
+ * the future ( if a row gets expensive can memo it ) 
  */
-const AuctionsListView = ({ charityId }: string) => {
+const AuctionListRowView = ({ auction }: I_AuctionRowModel) => {
 
-  {/* <ol>{charity.auctions.map(auction => (<li key={auction.auction_id}><h4>{auction.name}</h4><p>{auction.created_at}</p></li>))}</ol> */ }
+  const setSelectedAuction = (auctionId: string) => {
+    console.log("Clicked on Auction", auctionId);
+  }
 
   return (
+    <li className="bg-neutral-50 p-2 text-neutral-800 border-b">
+      <p className="text-base font-medium">{auction.name}</p>
+      <p className="text-xs font-regular text-neutral-500">auction is currently in: {auction.status} mode</p>
+      <p className="text-sm pt-2 pb-2">{auction.description}</p>
+      <div className="inline-block p2 border cursor-pointer" onClick={(e) => setSelectedAuction(auction.auction_id)}>
+        <p className="text-sm p-2 select-none">view bids</p>
+      </div>
+    </li>
+  )
+}
+
+/**
+ * AuctionsMiniListView
+ * Auctions Stubbed in with the full Auctions call
+ * TODO: will 
+ */
+const AuctionsMiniListView = ({ auctions }: I_AuctionRowCollection) => {
+  return (
     <div className="flex flex-col w-full pt-2">
-      <p className="text-xs text-neutral-800 pt-1">TMP AUCTIONS COLLECTION HERE</p>
+      <ul className="flex flex-col flex-grow bg-slate-100">
+        {auctions.map((auction) =>
+          <AuctionListRowView key={auction.auction_id} auction={auction} />
+        )}
+      </ul>
     </div>
   )
 }
@@ -52,10 +82,26 @@ const AuctionsListView = ({ charityId }: string) => {
  */
 const CharityDetails = ({ charity }: I_CharityModel) => {
 
-  // const totalRaised = charity.auctions.reduce((prior, current) => prior += current.high_bid_value ?? 0, 0)
+  const [auctionQuery, updateAuctionPagination] = useAuctionsQuery();
 
   const memberSinceDate = new Date(charity.created_at ?? '')
   const formattedMemberSinceDate = `${memberSinceDate.getMonth() + 1}/${memberSinceDate.getFullYear()}`
+
+  let totalRaised = 0;
+  let totalAuctions = 0;
+
+  // Skip the loading displays here to show the component prior to auctions request
+  // TODO: push down the auctionQuery.queryStatus into the child component to render
+  //       loading/error/isEmpty - this would make it look like partials
+  if (!auctionQuery.queryStatus.isLoading
+    && !auctionQuery.queryStatus.isError
+    && auctionQuery.auctions !== undefined) {
+    totalRaised = 26000;
+    totalAuctions = auctionQuery.auctions.length;
+  }
+
+  // Total Raised Math
+  // const totalRaised = charity.auctions.reduce((prior, current) => prior += current.high_bid_value ?? 0, 0)
 
   return (
     <div className="flex flex-col flex-grow bg-slate-50">
@@ -65,11 +111,13 @@ const CharityDetails = ({ charity }: I_CharityModel) => {
       </div>
       <div className="flex flex-row flex-grow flex-wrap w-full">
         {/* Left col */}
-        <div className="flex flex-col flex-shrink-0 flex-grow w-full border-r-none md:w-1/2 md:border-r p-2">
-          <p className="text-xs text-neutral-800 pt-1">Registered Charity Number (USA): {charity.ein}</p>
-          <p className="text-xs text-neutral-800 pt-1">contact: {charity.email}</p>
-          <p className="text-xs text-neutral-800 pt-1">Auctions [1]</p>
-          <AuctionsListView charityId={charity.charity_id} />
+        <div className="flex flex-col flex-shrink-0 flex-grow w-full border-r-none md:w-1/2 md:border-r">
+          <div className="p-2 border-b">
+            <p className="text-xs text-neutral-800 pt-1">Registered Charity Number (USA): {charity.ein}</p>
+            <p className="text-xs text-neutral-800 pt-1">contact: {charity.email}</p>
+            <p className="text-xs text-neutral-800 pt-1">Auctions [{totalAuctions}] Raised [${totalRaised}]</p>
+          </div>
+          <AuctionsMiniListView auctions={auctionQuery.auctions} />
         </div>
         {/* Right col */}
         <div className="flex flex-col flex-shrink-0 flex-grow w-full md:w-1/2 p-2">
@@ -88,6 +136,8 @@ export const CharityDetailPage = () => {
   // but they have typed it as string | string[] | undefined
   const charityId = router.query?.charityId as string;
   const query = useCharityQuery(charityId);
+
+  // These act as the "page getting data loaders"
 
   // ReactQuery Status -> loading 
   if (query.queryStatus.isLoading && query.queryStatus.isError === false) {
