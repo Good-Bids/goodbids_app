d3f113cff5bd669abdad75f123c0024f74f8f1b0
nameindustry <nameindustry@gmail.com>
2023-04-07 13:03:51 -0400

    feat - add in charities list page that links to charity details same as auctions

--- src/hooks/useCharity.ts
+++ src/hooks/useCharity.ts
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { updateCharityAdmin } from "./useCharityAdmin";
 import useSupabase from "./useSupabase";
@@ -57,4 +58,128 @@ export const useCreateCharity = (data: NewCharity) => {
       return updateData;
     },
   });
+};
+
+
+/**
+ * getCharities
+ * 
+ * ServerSide - false
+ * 
+ * Get a collection of Charities. Use Limit and range
+ * for pagination and also allow calling based on Status
+ * 
+ * Supabase call that fetches Via createBrowserSupabaseClient
+ * helper client from @supabase/auth-helpers-nextjs
+ * 
+ * #note: https://github.com/supabase/postgrest-js/commit/9df1e84750a2d83552d540e711c345b00f3ec1b3
+ * from above link - pulling out the returned value and allowing all errors to be surfaced
+ * 
+ * Using temporary mapped error code for inside app errors
+ * 5000 - getAuctions undefined error ( no code given by supabase )
+ * 
+ * Types are inferred from supabase DB <database>
+ * 
+ * Note: react query does not have classic query building so you can not build a 
+ * query and run it so we have to use an if
+ * 
+ */
+const getCharities = async (charityStatus = "ACTIVE", windowStart = 0, windowLength = 25) => {
+
+  try {
+
+    let result;
+
+    if (charityStatus === "*") {
+
+      result = await supabaseClient.from("charity")
+        .select()
+        .order('created_at', { ascending: false }) // Gets latest on top by creation date
+        .range(windowStart, windowLength)
+        .throwOnError();
+
+    } else {
+
+      result = await supabaseClient.from("charity")
+        .select()
+        .eq('status', charityStatus)
+        .order('created_at', { ascending: false }) // Gets latest on top by creation date
+        .range(windowStart, windowLength)
+        .throwOnError();
+
+    }
+
+    return {
+      status: result.status,
+      statusMessage: result.statusText,
+      charities: result.data,
+      hasError: false,
+      rawError: null,
+    }
+
+  } catch (err: any) {
+
+    return {
+      status: err?.code ?? "5000",
+      statusMessage: err?.message ?? "unknown error type",
+      charities: [],
+      hasError: true,
+      errorObj: err,
+    }
+
+  }
+
+};
+
+/**
+ * useCharitiesQuery
+ * 
+ * ServerSide - false
+ * 
+ * React hook that fetches Via createBrowserSupabaseClient 
+ * wrapped in ReactQuery.
+ * 
+ * React query returns required UI data on top of errors that can
+ * be surfaced. Since this is also wrapping the Supabase query and its 
+ * errors ( can be network or db related etc )
+ * 
+ * Error from ReactQuery is in error
+ * Error from SubQuery with Supabase is in data
+ * 
+ * return types are inferred
+ */
+export const useCharitiesQuery = (charityStatus?: string | undefined, windowStart = 0, windowLength = 0) => {
+
+  const [queryParameters, setQueryParameters] = useState({
+    charityStatus: charityStatus ?? "*",
+    windowStart: 0,
+    windowLength: 25,
+  });
+
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['auctionCollectionQueryResults',
+      queryParameters.charityStatus,
+      queryParameters.windowStart,
+      queryParameters.windowLength],
+    queryFn: async () => {
+      return await getCharities(
+        queryParameters.charityStatus,
+        queryParameters.windowStart,
+        queryParameters.windowLength);
+    }
+  });
+
+  return [
+    {
+      queryStatus: {
+        isLoading,
+        isError
+      },
+      charities: data?.charities ?? [],
+      hasError: (data?.hasError || isError) ? true : false,
+      errorMessage: (data?.hasError || isError) ? data?.statusMessage ?? "React Query encountered an error" : "",
+      errorObj: (data?.hasError || isError) ? data?.errorObj ?? error : null
+    },
+    setQueryParameters
+  ] as const
 };
