4be6a8ee669cc425e5992c72d629315f4cd8eb0e
Jasper Croome <jaspercroome@protonmail.com>
2023-04-07 20:33:55 -0700

    typescript adjustments

--- src/utils/types/auctions.ts
+++ src/utils/types/auctions.ts
@@ -1,56 +1,36 @@
 /**
- * File contains: 
- * 
+ * File contains:
+ *
  * Types and Interface implementations that cover usage
  * of the data layer <==> display layer
- * 
+ *
  */
-import { Database, Json } from "./supabase"
+import { Database, Json } from "./supabase";
 
 /** Convenience wrapper - from supabase.ts */
-export type T_AuctionModel = Database['public']['Tables']['auction']['Row'];
-export type T_AuctionBid = Database['public']['Tables']['bid']['Row'];
+export type T_AuctionModel = Database["public"]["Tables"]["auction"]["Row"];
+export type T_AuctionBid = Database["public"]["Tables"]["bid"]["Row"];
 
 export interface I_BidsCollection {
-    bids: T_AuctionBid[] | undefined
+  bids: T_AuctionBid[] | undefined;
 }
 
-/** 
- * Nested Types from Supabase require over rides in the return ( a cast ) 
+/**
+ * Nested Types from Supabase require over rides in the return ( a cast )
  * or make a new model manually. Probably the commented out cast in the useAuction
- * getAuctions is possible, but regardless the auctions need to be manually edited
- * to contain bids[]
+ * getAuctions is possible.
  */
-export type T_AuctionModelExtended = {
-    allowed_free_bids: string[] | null
-    auction_id: string
-    bid_currency: string | null
-    charity_id: string
-    created_at: string | null
-    description: string
-    high_bid_value: number | null
-    increment: number
-    item_id: string | null
-    minimum_bids: number | null
-    name: string
-    opening_bid_value: number
-    over_bid_good_time_active: boolean | null
-    over_bid_good_time_early_fee: number | null
-    over_bid_good_time_late_fee: number | null
-    over_bid_good_time_often_fee: number | null
-    over_bid_good_time_threshold_field: Json | null
-    status: string
-    top_bid_duration: number | null
-    type: string
-    bids?: I_BidsCollection
+
+export interface T_AuctionModelExtended extends T_AuctionModel {
+  bids?: I_BidsCollection;
 }
 
 /**
  * Below is the logical way to add the bids array
  * yet causes errors in in multiple files.
- * 
+ *
  * TODO: fix this properly
- * 
+ *
  */
 /*
 interface T_AuctionModelExtended extends T_AuctionModel {
@@ -59,10 +39,9 @@ interface T_AuctionModelExtended extends T_AuctionModel {
 */
 
 export interface I_AuctionModel {
-    auction: T_AuctionModelExtended
+  auction: T_AuctionModelExtended;
 }
 
 export interface I_AuctionCollection {
-    auctions: T_AuctionModel[]
+  auctions: T_AuctionModel[];
 }
-
--- src/utils/types/charities.ts
+++ src/utils/types/charities.ts
@@ -1,21 +1,29 @@
 /**
- * File contains: 
- * 
+ * File contains:
+ *
  * Types and Interface implementations that cover usage
  * of the data layer <==> display layer
- * 
+ *
  */
 
-import { Database } from "./supabase"
+import { Database } from "./supabase";
 
 /** Convenience wrappers - from supabase.ts */
-export type T_CharityModel = Database['public']['Tables']['charity']['Row'];
-export type T_NewCharityModel = Database['public']['Tables']['charity']['Insert']; // includes all schema
+export type T_CharityModel = Database["public"]["Tables"]["charity"]["Row"];
+export type T_NewCharityModel =
+  Database["public"]["Tables"]["charity"]["Insert"]; // includes all schema
 
 export interface I_CharityModel {
-    charity: T_CharityModel
+  charity: T_CharityModel;
 }
 
 export interface I_CharityCollection {
-    charities: T_CharityModel[] | []
+  charities: T_CharityModel[] | [];
+}
+
+export interface I_CreateCharityFormValues {
+  email: string;
+  ein: string;
+  name: string;
+  adminId: string;
 }
