97e46ee8b963c5771f71e3ecbb51e00c90c13eff
Jasper Croome <jaspercroome@protonmail.com>
2023-04-14 05:54:52 -0700

    minor tweaks

--- src/features/auctions/PayPalDialog.tsx
+++ src/features/auctions/PayPalDialog.tsx
@@ -29,6 +29,7 @@ import {
 
 import * as ga from "../../lib/ga";
 import { useUserQuery } from "~/hooks/useUser";
+import { useRouter } from "next/router";
 
 interface PayPalDialogProps {
   bidValue: number;
@@ -37,20 +38,23 @@ interface PayPalDialogProps {
 
 export const PayPalDialog = ({ bidValue, auction }: PayPalDialogProps) => {
   const userJWT = useUserQuery();
+  const router = useRouter();
 
   // State of the Dialog
-  const [isDialogOpen, updateDialogState] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   // When the paypal button is clicked it signals to locks the
   // bidding for everyone and saves the lockId here
-  const [bidLockId, updateBidLockId] = useState();
+  const [bidLockId, setBidLockId] = useState<string>();
 
   // Track paypal calls
-  const [paypalState, updatePaypalState] = useState("");
+  const [paypalState, setPaypalState] = useState<
+    "COMPLETED" | "CANCELLED" | "CREATE_ORDER" | "NOT_STARTED" | "ERROR"
+  >("NOT_STARTED");
 
   // Open the bid now dialog and track its opening via google
   const openBidDialog = () => {
-    updateDialogState(true);
+    setIsDialogOpen(true);
     // place the event tag inside the open dialog
     // otherwise you are only capturing pageLoad
     // not "button_click"
@@ -65,9 +69,7 @@ export const PayPalDialog = ({ bidValue, auction }: PayPalDialogProps) => {
   // TODO: cancel and reset everything if user
   //       mistakenly hits the close via the
   //       dialog background
-  useEffect(() => {
-    
-  }, [isDialogOpen]);
+  useEffect(() => {}, [isDialogOpen]);
 
   // Rough state control flow for synchronizing the
   // paypal 3rd party system to the supabase system
@@ -89,7 +91,7 @@ export const PayPalDialog = ({ bidValue, auction }: PayPalDialogProps) => {
         bidValue
       );
 
-      updateBidLockId(updateBidTableResults.bid[0].bid_id);
+      setBidLockId(updateBidTableResults.bid[0].bid_id);
 
       // Check all three calls to make sure things are smooth
       // then update the component. We really need a "cancel"
@@ -111,8 +113,11 @@ export const PayPalDialog = ({ bidValue, auction }: PayPalDialogProps) => {
       // Check all three calls to make sure things are smooth
       // then update the component. We really need a "cancel"
       // paypal flow surfaced from the 3rd party component here
+      setBidLockId(undefined);
 
-      updateBidLockId(undefined);
+      // on complete, close the dialog and refresh the page to show the new amount on the button
+      setIsDialogOpen(false);
+      router.reload();
     };
 
     // if a user Id provided is undefined
@@ -139,7 +144,7 @@ export const PayPalDialog = ({ bidValue, auction }: PayPalDialogProps) => {
     data: CreateOrderData,
     actions: CreateOrderActions
   ) => {
-    updatePaypalState("CREATE_ORDER");
+    setPaypalState("CREATE_ORDER");
     return await actions.order?.create({
       purchase_units: [
         {
@@ -156,12 +161,12 @@ export const PayPalDialog = ({ bidValue, auction }: PayPalDialogProps) => {
     data: OnApproveData,
     actions: OnApproveActions
   ) => {
-    updatePaypalState("COMPLETED");
+    setPaypalState("COMPLETED");
     const details = await actions.order?.capture();
-    const name = details?.payer?.name?.given_name ?? "an unknown GoodBidder"; // because capture() can be promise | undefined
+    const name = details?.payer?.name?.given_name ?? ""; // because capture() can be promise | undefined
 
     // this is where we'll do more with supabase on success
-    alert(`Transaction completed by ${name}`);
+    alert(`GoodBid confirmed, thank you${name ? ` ${name}` : ""}!`);
   };
 
   // paypal specific method
@@ -170,23 +175,23 @@ export const PayPalDialog = ({ bidValue, auction }: PayPalDialogProps) => {
     actions: OnCancelledActions
   ) => {
     // this is where we'll update bid state to cancelled
-    updatePaypalState("CANCELLED");
+    setPaypalState("CANCELLED");
     console.log("PP: cancel triggered", data);
   };
 
   // paypal specific method
   const handleError = async (error: Record<string, unknown>) => {
     // this is where we'll update bid state to error
-    updatePaypalState("ERROR");
+    setPaypalState("ERROR");
     console.log("PP: error triggered", error);
   };
 
   return (
-    <Dialog open={isDialogOpen} onOpenChange={updateDialogState}>
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <DialogTrigger asChild>
         <div
           id="call-to-action"
-          className="flex flex-col justify-center pt-4 pb-4 min-h-fit w-fit"
+          className="flex min-h-fit w-fit flex-col justify-center pb-4 pt-4"
         >
           <button
             className={`container rounded-full bg-bottleGreen px-8 py-4 text-xl font-bold text-hintOfGreen`}
