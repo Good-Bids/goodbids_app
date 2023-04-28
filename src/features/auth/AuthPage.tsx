0989bf749f59884b943363f66c8410649ba30553
jaspercroome <128058464+jaspercroome@users.noreply.github.com>
2023-04-28 12:00:07 -0700

    Test (#35)
    * change debug
    
    * change debug, again
    
    * remove debug <p>s

--- src/features/auth/AuthPage.tsx
+++ src/features/auth/AuthPage.tsx
@@ -21,8 +21,8 @@ export const AuthPage = ({ method }: AuthPageProps) => {
 
   const getURL = () => {
     let url =
-      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
       process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+      process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
       "http://localhost:3000/";
     // Make sure to include `https://` when not localhost.
     url = url.includes("http") ? url : `https://${url}`;
@@ -83,7 +83,6 @@ export const AuthPage = ({ method }: AuthPageProps) => {
             <p className="block text-sm font-bold text-slate-700">
               {`${actionString} with email`}
             </p>
-            <p>FOR TESTING ONLY - {getURL()}</p>
             <input
               type={"email"}
               name={"email"}
