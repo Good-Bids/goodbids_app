e41ff937d0cf613cef28ff6770bcda6a175ab9eb
Jasper Croome <jaspercroome@protonmail.com>
2023-04-28 08:09:53 -0700

    adding react import

--- src/features/auth/LogInPage.tsx
+++ src/features/auth/LogInPage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { AuthPage } from "./AuthPage";
 
 export const LoginPage = () => <AuthPage method="Login" />;
--- src/features/auth/SignupPage.tsx
+++ src/features/auth/SignupPage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { AuthPage } from "./AuthPage";
 
 export const SignupPage = () => <AuthPage method="Signup" />;
