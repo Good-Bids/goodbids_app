72900d329c2aa2b4f18b86f5e7e4788e5b5859ee
Jasper Croome <jaspercroome@protonmail.com>
2023-07-12 06:45:16 -0700

    text stylinh

--- src/features/auctions/ShareButton.tsx
+++ src/features/auctions/ShareButton.tsx
@@ -58,8 +58,12 @@ export const ShareButton = ({ userId }: ShareButtonProps) => {
         </div>
       ) : (
         <>
-          <p>Click the button below to create a referral link.</p>
-          <p>If two people sign up using your link, you'll earn a free bid!</p>
+          <p className="text-center text-lg font-bold text-cw-blue">
+            Click the button below to create a referral link.
+          </p>
+          <p className="text-center text-sm font-light text-outerSpace-700">
+            If two people sign up using your link, you'll earn a free bid!
+          </p>
           <button
             className="container w-full rounded bg-green-500 py-2 text-xl font-bold text-white"
             onClick={handleCreateClick}
