fd9d0be17d13110397d8a2982470f996bd744e57
Jasper Croome <jaspercroome@protonmail.com>
2023-07-20 06:57:05 -0700

    dynamic redirect at "/"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useAuctionRedirect } from "~/hooks/useAuctionRedirect";
 
@@ -6,22 +7,31 @@ const Home = () => {
 
   return (
     <div className="flex h-full w-11/12 flex-col items-center justify-center gap-4">
-      <p className="self-center text-4xl font-bold text-black">
-        Welcome to GoodBids
-      </p>
-      <div className="flex h-fit w-full flex-col flex-wrap justify-center gap-16 md:h-full md:flex-row">
-        <Link href="/trek-intro" className="h-fit">
-          <div
-            className={`mx-6 flex h-fit w-full justify-center rounded bg-bo-red p-8 md:h-fit md:w-fit`}
-          >
-            <p className="text-2xl font-bold text-white">The Trek Auction</p>
+      <div className="flex h-full w-full flex-col flex-wrap justify-center gap-16 sm:h-full sm:flex-row">
+        <Link href="/trek-intro" className="h-fit w-full sm:w-2/5">
+          <p className="text-center text-xl font-bold text-bo-red">
+            The Trek Auction
+          </p>
+          <div className=" relative flex aspect-video w-full flex-col items-center gap-2">
+            <Image
+              src="/buildOn_senegal-2.webp"
+              alt="a buildOn trek member shares a moment with a student"
+              fill
+              style={{ objectFit: "contain" }}
+            />
           </div>
         </Link>
-        <Link href="/watch-intro" className="h-fit">
-          <div
-            className={`mx-6 flex h-fit w-full justify-center rounded bg-bo-red p-8 md:h-fit md:w-fit`}
-          >
-            <p className="text-2xl font-bold text-white">The Watch Auction</p>
+        <Link href="/watch-intro" className="h-fit w-full sm:w-2/5">
+          <p className="text-center text-xl font-bold text-bo-red">
+            The Watch Auction
+          </p>
+          <div className=" relative flex aspect-video w-full flex-col items-center gap-2">
+            <Image
+              src="/watch_hero.jpg"
+              fill
+              style={{ objectFit: "contain" }}
+              alt="an image of the apple watch ultra"
+            />
           </div>
         </Link>
       </div>
