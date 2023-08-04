dae6663b735735ee3de28926683ca61b4ab00067
Jasper Croome <jaspercroome@protonmail.com>
2023-08-03 20:29:05 -0700

    disable credit, card

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -5,7 +5,8 @@ export const initialOptions: PayPalScriptOptions = {
   clientId: env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
   currency: "USD",
   intent: "capture",
-  // debug: true
+  disableFunding: "credit,card",
+  debug: true,
 };
 
 export const prizeVideoUrls = {
