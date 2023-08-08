8ea0c83c2518b88675fe94ffed62391ff5af5e6d
Jasper Croome <jaspercroome@protonmail.com>
2023-08-08 15:44:25 -0700

    add card payment back in

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -5,7 +5,7 @@ export const initialOptions: PayPalScriptOptions = {
   clientId: env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
   currency: "USD",
   intent: "authorize",
-  disableFunding: "credit,card",
+  disableFunding: "credit",
   commit: true,
   debug: false,
 };
