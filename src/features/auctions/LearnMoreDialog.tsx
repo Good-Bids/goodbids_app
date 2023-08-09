654116942e9a652b63ef216f2bcc52c8877abd70
Jasper Croome <jaspercroome@protonmail.com>
2023-08-09 12:41:10 -0700

    this feels better

---
+++ src/features/auctions/LearnMoreDialog.tsx
@@ -0,0 +1,54 @@
+import Link from "next/link";
+import { useState } from "react";
+import { ArrowRight } from "lucide-react";
+import { Dialog, DialogTrigger, DialogContent } from "src/components/Dialog";
+import { goodBidsIntroVideo } from "~/utils/constants";
+
+export const LearnMoreDialog = () => {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const handleOpenChange = async (changeIsOpenTo: boolean) => {
+    switch (changeIsOpenTo) {
+      case true:
+        {
+          setIsDialogOpen(changeIsOpenTo);
+        }
+        break;
+      case false: {
+        console.log("closing");
+        setIsDialogOpen(changeIsOpenTo);
+      }
+    }
+  };
+  const openLearnMoreDialog = () => {
+    setIsDialogOpen(true);
+  };
+
+  return (
+    <Dialog open={isDialogOpen} onOpenChange={handleOpenChange}>
+      <DialogTrigger asChild>
+        <p
+          className="mb-2 flex w-fit cursor-pointer self-center rounded bg-bo-red px-4 py-1 text-center text-lg font-bold text-white"
+          onClick={openLearnMoreDialog}
+        >
+          Learn More about GoodBids
+        </p>
+      </DialogTrigger>
+      <DialogContent className="sm:w-80% h-fit sm:max-w-[1200px]">
+        <div
+          className={`mt-4 flex aspect-video h-fit w-full flex-col items-center gap-4 px-1 py-2 sm:px-2 sm:py-4`}
+        >
+          <p className="text-center text-lg font-bold text-bo-red">
+            Watch this video to learn more about GoodBids
+          </p>
+          <iframe
+            src={goodBidsIntroVideo}
+            allow="fullscreen; picture-in-picture; autoplay"
+            className="mb-2 h-full sm:w-full"
+            title="buildOn and GoodBids"
+          />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { LearnMoreDialog } from "~/features/auctions/LearnMoreDialog";
 import {
-  goodBidsIntroVideo,
   prizeShortDescriptions,
   prizeValue,
   prizeVideoUrls,
@@ -11,7 +11,6 @@ import {
 const Home = () => {
   const router = useRouter();
   const [showHome, setShowHome] = useState(false);
-  const [introVideoDimensions, setIntroVideoDimensions] = useState("0px");
   useEffect(() => {
     if (window) {
       // if the user has come from the redirect page
@@ -38,7 +37,7 @@ const Home = () => {
   const prizes = ["Trek", "Watch"];
 
   return (
-    <div className="flex h-full w-11/12 flex-col flex-nowrap items-center justify-center">
+    <div className="flex h-fit w-11/12 flex-col flex-nowrap items-center justify-center sm:h-full">
       {showHome && (
         <div className="align-center flex h-fit w-full flex-col flex-nowrap justify-start gap-8 overflow-y-auto sm:h-fit sm:flex-row sm:flex-wrap sm:items-center sm:justify-center sm:gap-12">
           {prizes.map((prize) => {
@@ -73,32 +72,7 @@ const Home = () => {
               </Link>
             );
           })}
-          <div
-            className={`mt-4 flex aspect-video h-fit w-full flex-col items-center sm:mb-0 ${
-              introVideoDimensions === "0px" ? "sm:w-fit" : "sm:w-1/2"
-            }`}
-          >
-            <iframe
-              src={goodBidsIntroVideo}
-              allow="fullscreen; picture-in-picture; autoplay"
-              className={introVideoDimensions === "0px" ? "hidden" : "visible"}
-              style={{
-                height: "100%",
-                width: "100%",
-              }}
-              title="buildOn and GoodBids"
-            />
-            <p
-              className=" cursor-pointer text-lg font-bold text-bo-red underline"
-              onClick={() =>
-                setIntroVideoDimensions((prior) =>
-                  prior === "0px" ? "100%" : "0px"
-                )
-              }
-            >
-              Lean More about GoodBids
-            </p>
-          </div>
+          <LearnMoreDialog />
         </div>
       )}
     </div>
