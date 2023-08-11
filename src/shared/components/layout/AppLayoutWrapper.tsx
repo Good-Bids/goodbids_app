fe2dccd624565c9a9632cf8516a12de2da7176a0
jaspercroome <128058464+jaspercroome@users.noreply.github.com>
2023-08-10 18:16:33 -0700

    Show footer (#78)
    * trying out svh
    
    * added calc()
    
    * added borders for debug
    
    * removed borders

--- src/shared/components/layout/AppLayoutWrapper.tsx
+++ src/shared/components/layout/AppLayoutWrapper.tsx
@@ -37,7 +37,7 @@ export const AppLayoutWrapper = ({ children }: WrapperProps) => {
   }, []);
 
   return (
-    <div className={`flex h-[${innerHeight}] w-full flex-col`}>
+    <div className={`flex h-[calc(100svh)] w-full flex-col`}>
       <div className="fixed left-0 right-0 top-0 z-10 flex h-[72px] w-full flex-col items-center shadow">
         <div
           id="appLayoutWrapperHead"
@@ -73,11 +73,9 @@ export const AppLayoutWrapper = ({ children }: WrapperProps) => {
       </div>
       <main
         id="appLayoutWrapperMain"
-        className="fixed left-0 right-0 top-[72px] z-0 mx-0 flex h-[calc(100vh_-_72px)] flex-col items-center justify-start self-center overflow-y-auto sm:left-[50%] sm:w-full sm:max-w-[1440px] sm:translate-x-[-50%] sm:px-4"
+        className="fixed left-0 right-0 top-[72px] z-0 mx-0 flex h-[calc(100svh_-_144px)] flex-col items-center justify-start self-center overflow-y-auto sm:left-[50%] sm:w-full sm:max-w-[1440px] sm:translate-x-[-50%] sm:px-4"
       >
-        <div className="h-[calc(100%_-_144px)] w-full sm:h-[calc(100%_-_72px)]">
-          {children}
-        </div>
+        <div className="h-full w-full">{children}</div>
       </main>
       <div
         id="appLayoutWrapperFooter"
