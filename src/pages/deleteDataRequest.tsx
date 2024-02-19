bf8defb56987c203c2a9befb118d796abafc9746
Jasper Croome <jaspercroome@protonmail.com>
2024-02-19 06:03:39 -0800

    add DDP

---
+++ src/pages/deleteDataRequest.tsx
@@ -0,0 +1,13 @@
+const DeleteDataRequest = () => {
+  return (
+    <div className="flex h-full w-full flex-col items-start justify-center gap-2 text-bottleGreen">
+      <h1 className=" text-4xl font-black text-bottleGreen">
+        Data Deletion Request
+      </h1>
+      <p>To delete your data, please reach out to:</p>
+      <a href="mailto:support@goodbids.org">support@goodbids.org</a>
+    </div>
+  );
+};
+
+export default DeleteDataRequest;
